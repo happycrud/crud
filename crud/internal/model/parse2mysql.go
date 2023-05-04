2680fa72e8011e903705c819c8f327eae47baa8a
hongshengjie <hongshengjie@MacBookPro.local>
2023-05-04 17:02:50 +0800

    use pingcap mysql parser

--- crud/internal/model/parse2mysql.go
+++ crud/internal/model/parse2mysql.go
@@ -1,96 +1,37 @@
 package model
 
 import (
-	"io/ioutil"
 	"log"
+	"os"
 	"strings"
 
-	"github.com/xwb1989/sqlparser"
+	"github.com/pingcap/parser"
+	"github.com/pingcap/parser/ast"
+	"github.com/pingcap/parser/format"
+	"github.com/pingcap/parser/mysql"
+	"github.com/pingcap/parser/test_driver"
+	"github.com/pingcap/parser/types"
 )
 
-func MysqlColumn(ddl *sqlparser.DDL, notint64 bool) ([]*Column, error) {
-
-	res := []*Column{}
-	for k, v := range ddl.TableSpec.Columns {
-
-		var ct string
-		if v.Type.Unsigned {
-			ct = "unsigned"
-		}
-		var dct bool
-		if v.Type.Default != nil {
-			if string(v.Type.Default.Val) == "current_timestamp" || string(v.Type.Default.Val) == "current_timestamp()" {
-				dct = true
-			}
-		}
-		var comment string
-		if v.Type.Comment != nil {
-			comment = string(v.Type.Comment.Val)
-		}
-
-		c := &Column{
-			OrdinalPosition:           k,
-			ColumnName:                v.Name.String(),
-			DataType:                  v.Type.Type,
-			ColumnType:                ct,
-			ColumnComment:             comment,
-			NotNull:                   bool(v.Type.NotNull),
-			IsPrimaryKey:              false,
-			IsAutoIncrment:            bool(v.Type.Autoincrement),
-			IsDefaultCurrentTimestamp: dct,
-			GoColumnName:              "",
-			GoColumnType:              "",
-			BigType:                   0,
-			GoConditionType:           "",
-		}
-
-		c.GoColumnName = GoCamelCase(c.ColumnName)
-		c.GoColumnType, c.BigType = MysqlToGoFieldType(c.DataType, c.ColumnType)
-		if strings.Contains(c.GoColumnType, "int") && !notint64 {
-			c.GoColumnType = "int64"
-		}
-		c.GoConditionType = c.GoColumnType
-		if c.BigType == bigtypeCompareTime {
-			c.GoConditionType = "string"
-		}
-		res = append(res, c)
-
-	}
-	var primaryKey string
-	for _, v := range ddl.TableSpec.Indexes {
-		if v.Info.Primary {
-			if len(v.Columns) != 1 {
-				log.Fatal("primary key must be one column")
-			}
-			primaryKey = v.Columns[0].Column.String()
-		}
-	}
-	for _, v := range res {
-		if v.ColumnName == primaryKey {
-			v.IsPrimaryKey = true
-		}
-		v.ProtoType = GoTypeToProtoType(v.GoColumnType)
-	}
-	return res, nil
-}
-
 func MysqlTable(db, path, relative string, notint64 bool, dialect string) *Table {
-	sql, err := ioutil.ReadFile(path)
+	sql, err := os.ReadFile(path)
 	if err != nil {
 		log.Fatal(err)
 	}
-	stmt, err := sqlparser.ParseStrictDDL(trimTimeStampFunc(string(sql)))
+
+	p, _, err := parser.New().Parse(string(sql), "", "")
 	if err != nil {
 		log.Fatal(err)
 	}
-	ddl, ok := stmt.(*sqlparser.DDL)
+	stmt, ok := p[0].(*ast.CreateTableStmt)
 	if !ok {
-		log.Fatal("please check sql file statement is DDL ")
+		log.Fatal("please check sql file statement is DDL")
 	}
-	if ddl.Action != sqlparser.CreateStr {
-		log.Fatal("please check sql file statement is DDL and action is create  ")
+	var buf strings.Builder
+	if err = stmt.Restore(format.NewRestoreCtx(format.DefaultRestoreFlags, &buf)); err != nil {
+		log.Fatal(err)
 	}
-	tableName := ddl.NewName.Name.String()
+	tableName := stmt.Table.Name.String()
 
 	gotableName := GoCamelCase(tableName)
 	mytable := &Table{
@@ -100,7 +41,7 @@ func MysqlTable(db, path, relative string, notint64 bool, dialect string) *Table
 		PackageName: strings.ToLower(gotableName),
 		Dialect:     dialect,
 	}
-	columns, err := MysqlColumn(ddl, notint64)
+	columns, err := MysqlColumn(stmt, notint64)
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -122,4 +63,148 @@ func MysqlTable(db, path, relative string, notint64 bool, dialect string) *Table
 	mytable.RelativePath = relative
 	return mytable
 
+}
+
+func MysqlColumn(ddl *ast.CreateTableStmt, notint64 bool) ([]*Column, error) {
+
+	res := []*Column{}
+	for k, v := range ddl.Cols {
+
+		var ct string
+		if mysql.HasUnsignedFlag(v.Tp.Flag) {
+			ct = "unsigned"
+		}
+		var defaultTs bool
+		var notNull bool
+		var autoIncrement bool
+		var primaryKey bool
+		var comment string
+		for _, v2 := range v.Options {
+			switch v2.Tp {
+			case ast.ColumnOptionDefaultValue:
+				if v2.Expr.GetFlag() == ast.FlagHasFunc && v2.Expr.(*ast.FuncCallExpr).FnName.L == "current_timestamp" {
+					defaultTs = true
+				}
+			case ast.ColumnOptionComment:
+				comment = v2.Expr.(*test_driver.ValueExpr).GetString()
+			case ast.ColumnOptionNotNull:
+				notNull = true
+			case ast.ColumnOptionAutoIncrement:
+				autoIncrement = true
+			case ast.ColumnOptionPrimaryKey:
+				primaryKey = true
+			default:
+			}
+		}
+		columnType := types.TypeToStr(v.Tp.Tp, v.Tp.Charset)
+		c := &Column{
+			OrdinalPosition:           k,
+			ColumnName:                v.Name.Name.L,
+			DataType:                  columnType,
+			ColumnType:                ct,
+			ColumnComment:             comment,
+			NotNull:                   notNull,
+			IsPrimaryKey:              primaryKey,
+			IsAutoIncrment:            autoIncrement,
+			IsDefaultCurrentTimestamp: defaultTs,
+			GoColumnName:              "",
+			GoColumnType:              "",
+			BigType:                   0,
+			GoConditionType:           "",
+		}
+
+		c.GoColumnName = GoCamelCase(c.ColumnName)
+		c.GoColumnType, c.BigType = MysqlToGoFieldType(c.DataType, c.ColumnType)
+		if strings.Contains(c.GoColumnType, "int") && !notint64 {
+			c.GoColumnType = "int64"
+		}
+		c.GoConditionType = c.GoColumnType
+		if c.BigType == bigtypeCompareTime {
+			c.GoConditionType = "string"
+		}
+		res = append(res, c)
+
+	}
+	var primaryKey string
+	for _, v := range ddl.Constraints {
+		if v.Tp == ast.ConstraintPrimaryKey {
+			if len(v.Keys) > 1 {
+				log.Fatal("primary key is not single column")
+			}
+			primaryKey = v.Keys[0].Column.Name.String()
+		}
+	}
+	for _, v := range res {
+		if v.ColumnName == primaryKey {
+			v.IsPrimaryKey = true
+		}
+		v.ProtoType = GoTypeToProtoType(v.GoColumnType)
+	}
+	return res, nil
+}
+
+// MysqlToGoFieldType MysqlToGoFieldType
+func MysqlToGoFieldType(dt, ct string) (string, int) {
+	var unsigned bool
+	if strings.Contains(ct, "unsigned") {
+		unsigned = true
+	}
+	var typ string
+	var gtp int
+	switch dt {
+	case "bit":
+		typ = "[]byte"
+		gtp = bigtypeCompareBit
+	case "bool", "boolean":
+		typ = "bool"
+	case "char", "varchar":
+		typ = "string"
+		gtp = bigtypeCompareString
+	case "tinytext", "text", "mediumtext", "longtext", "json":
+		typ = "string"
+	case "tinyint":
+		typ = "int8"
+		if unsigned {
+			typ = "uint8"
+		}
+		gtp = bigtypeCompare
+	case "smallint":
+		typ = "int16"
+		if unsigned {
+			typ = "uint16"
+		}
+		gtp = bigtypeCompare
+	case "mediumint", "int", "integer":
+		typ = "int32"
+		if unsigned {
+			typ = "uint32"
+		}
+		gtp = bigtypeCompare
+	case "bigint":
+		typ = "int64"
+		if unsigned {
+			typ = "uint64"
+		}
+		gtp = bigtypeCompare
+	case "float":
+		typ = "float32"
+		gtp = bigtypeCompare
+	case "decimal", "double":
+		typ = "float64"
+		gtp = bigtypeCompare
+	case "binary", "varbinary":
+		typ = "[]byte"
+		gtp = bigtypeCompare
+	case "tinyblob", "blob", "mediumblob", "longblob":
+		typ = "[]byte"
+	case "timestamp", "datetime", "date":
+		typ = "time.Time"
+		gtp = bigtypeCompareTime
+	case "time", "year", "enum", "set":
+		typ = "string"
+		gtp = bigtypeCompare
+	default:
+		typ = "UNKNOWN"
+	}
+	return typ, gtp
 }
--- crud/internal/model/parse2sqlite.go
+++ crud/internal/model/parse2sqlite.go
@@ -1,11 +1,9 @@
 package model
 
-import "github.com/xwb1989/sqlparser"
-
 func Sqlite3Table(db, path, relative string, notint64 bool, dialect string) *Table {
 	return nil
 }
 
-func Sqlite3Column(ddl *sqlparser.DDL, notint64 bool) ([]*Column, error) {
+func Sqlite3Column(ddl string, notint64 bool) ([]*Column, error) {
 	return nil, nil
 }
