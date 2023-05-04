662e8e139c91e6a0633c1864c8cf3188e8c215c4
hongshengjie <hongshengjie@MacBookPro.local>
2023-05-04 16:29:19 +0800

    add parser test func

--- crud/internal/model/util_test.go
+++ crud/internal/model/util_test.go
@@ -2,7 +2,15 @@ package model
 
 import (
 	"fmt"
+	"log"
+	"strings"
 	"testing"
+
+	pg_query "github.com/pganalyze/pg_query_go/v4"
+	"github.com/pingcap/parser"
+	"github.com/pingcap/parser/ast"
+	_ "github.com/pingcap/parser/test_driver"
+	"github.com/rqlite/sql"
 )
 
 func TestGoModFilePath(t *testing.T) {
@@ -10,4 +18,69 @@ func TestGoModFilePath(t *testing.T) {
 	got := GoModFilePath()
 	fmt.Println(got)
 
+}
+
+const user_table_sqlite = `
+CREATE TABLE "user" (
+	"id" integer NOT NULL,
+	"name" text NOT NULL,
+	"age" integer NOT NULL,
+	"ctime" integer NOT NULL,
+	"mtime" integer NOT NULL,
+	PRIMARY KEY ("id")
+  );
+`
+
+const user_table_pg = `
+CREATE TABLE "public"."user" (
+	"id" int8 NOT NULL DEFAULT nextval('user_id_seq'::regclass),
+	"name" varchar(255) COLLATE "pg_catalog"."default" NOT NULL,
+	"age" int4 NOT NULL,
+	"ctime" timestamp(6) NOT NULL DEFAULT now(),
+	"mtime" timestamp(6) NOT NULL DEFAULT now(),
+	CONSTRAINT "user_pkey" PRIMARY KEY ("id")
+  )
+  ;
+`
+
+const user_table_mysql = `
+CREATE TABLE user (
+    id int(10) unsigned NOT NULL AUTO_INCREMENT COMMENT 'id字段',
+    name varchar(100) NOT NULL COMMENT '名称',
+    age int(11) NOT NULL DEFAULT '0' COMMENT '年龄',
+    ctime datetime NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+    mtime datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间',
+    PRIMARY KEY (id),
+    KEY ix_name (name) USING BTREE,
+    KEY ix_mtime (mtime) USING BTREE
+) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4
+`
+
+func TestSqliteParse(t *testing.T) {
+	p := sql.NewParser(strings.NewReader(user_table_sqlite))
+	st, err := p.ParseStatement()
+	if err != nil {
+		panic(err)
+	}
+	ct := st.(*sql.CreateTableStatement)
+	fmt.Println(ct.String(), ct.Columns)
+	fmt.Println(ct.Constraints)
+}
+
+func TestPgParse(t *testing.T) {
+	x, err := pg_query.Parse(user_table_pg)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(x.String())
+
+}
+
+func TestMysqlParse(t *testing.T) {
+	p, _, err := parser.New().Parse(user_table_mysql, "", "")
+	if err != nil {
+		log.Fatal(err)
+	}
+	tableStmt, ok := p[0].(*ast.CreateTableStmt)
+	fmt.Println(tableStmt, ok)
 }
--- go.mod
+++ go.mod
@@ -12,9 +12,31 @@ require (
 )
 
 require (
+	github.com/golang/protobuf v1.4.2 // indirect
+	github.com/pganalyze/pg_query_go/v4 v4.2.0 // indirect
+	google.golang.org/protobuf v1.23.0 // indirect
+)
+
+require (
+	github.com/cznic/mathutil v0.0.0-20181122101859-297441e03548 // indirect
 	github.com/jackc/pgpassfile v1.0.0 // indirect
 	github.com/jackc/pgservicefile v0.0.0-20221227161230-091c0ba34f0a // indirect
+	github.com/pingcap/errors v0.11.5-0.20210425183316-da1aaba5fb63 // indirect
+	github.com/pingcap/log v0.0.0-20210625125904-98ed8e2eb1c7 // indirect
+	github.com/pingcap/parser v0.0.0-20220622031236-3bca03d3057b // indirect
+	github.com/pingcap/tipb v0.0.0-20230427024529-aed92caf20b9 // indirect
+	github.com/remyoudompheng/bigfft v0.0.0-20200410134404-eec4a21b6bb0 // indirect
 	github.com/rogpeppe/go-internal v1.10.0 // indirect
+	github.com/rqlite/sql v0.0.0-20221103124402-8f9ff0ceb8f0 // indirect
+	github.com/sirupsen/logrus v1.9.0 // indirect
+	go.uber.org/atomic v1.7.0 // indirect
+	go.uber.org/multierr v1.6.0 // indirect
+	go.uber.org/zap v1.18.1 // indirect
+	golang.org/x/net v0.6.0 // indirect
+	golang.org/x/sys v0.5.0 // indirect
+	google.golang.org/genproto v0.0.0-20180817151627-c66870c02cf8 // indirect
+	google.golang.org/grpc v1.24.0 // indirect
+	gopkg.in/natefinch/lumberjack.v2 v2.0.0 // indirect
 )
 
 require (
