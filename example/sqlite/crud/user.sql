cedb3e0d55d76fe13768085a609ddcf831fa1565
hongshengjie <hongshengjie@MacBookPro.local>
2023-05-12 19:13:41 +0800

    mv example dir

--- example/sqlite/crud/user.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE "user" (
-    "id" integer NOT NULL,
-    "name" text NOT NULL,
-    "age" integer NOT NULL,
-    "ctime" integer NOT NULL,
-    "mtime" integer NOT NULL,
-    PRIMARY KEY ("id")
-);
